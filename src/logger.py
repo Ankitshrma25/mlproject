e26da27f7209ee5e11e1cdfa9624e3f4b2d94f9c
Ankit Sharma <116503887+Ankitshrma25@users.noreply.github.com>
2025-12-14 18:39:23 +0530

    Completed Project Structure

--- src/logger.py
+++ src/logger.py
@@ -3,17 +3,17 @@ import logging
 import os
 from datetime import datetime
 
-# Creating log file
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-Logs_path=os.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(Logs_path,exist_ok=True)
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(Logs_path,LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s -%(levelname)s- [%(message)s]",
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 
 
 )
+
